e2c6fa7e933d56857ad9f4ebcc81b8a862381803
AdiltonSantosdeMorais <94267843+AdiltonSantosdeMorais@users.noreply.github.com>
2026-05-18 21:24:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "app.py",
-      "use": "@vercel/python"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/sw.js",
-      "dest": "/sw.js"
-    },
-    {
-      "src": "/manifest.json",
-      "dest": "/static/manifest.json"
-    },
-    {
-      "src": "/static/(.*)",
-      "dest": "/static/$1"
-    },
-    {
-      "src": "/(.*)",
-      "dest": "app.py"
-    }
-  ]
-}
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sw.js')
+def service_worker():
+    return send_from_directory(os.path.abspath(os.path.dirname(__file__)), 'sw.js', mimetype='application/javascript')
+
+@app.route('/manifest.json')
+def manifest():
+    return send_from_directory(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static'), 'manifest.json', mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(debug=True)
