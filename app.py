9d083fd78f16533763a1b3de9c0dcc40ff3ce986
AdiltonSantosdeMorais <94267843+AdiltonSantosdeMorais@users.noreply.github.com>
2026-05-19 19:53:10 -0400

    rd

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, send_from_directory
 import os
 
-# Força o Flask a reconhecer a pasta static de forma absoluta
+# Força o Flask a mapear a pasta static explicitamente
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/')
